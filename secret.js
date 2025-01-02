0fbbb4e27c93387ee939eabc358846013017c0d0
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-01-02 21:39:10 +0500

    done

---
+++ secret.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBcy_8FsOtVJhniYbzc1n7yi5fiSYPmxyw",
+  authDomain: "code-connect-authentication.firebaseapp.com",
+  projectId: "code-connect-authentication",
+  storageBucket: "code-connect-authentication.firebasestorage.app",
+  messagingSenderId: "575198466241",
+  appId: "1:575198466241:web:369656e056e8aa55ef0440",
+};
